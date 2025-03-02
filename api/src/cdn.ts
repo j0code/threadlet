bc46429e4d0c9325d2c401c43e4566982b1a7a1f
j0code <42189560+j0code@users.noreply.github.com>
2025-03-02 01:45:47 +0100

    split api from client

---
+++ api/src/cdn.ts
@@ -0,0 +1,17 @@
+import { CDN as DiscordCDN } from "@discordjs/rest"
+
+const cdn = new DiscordCDN()
+
+export default class CDN {
+
+	static avatar(userId: string, avatarHash: string | null): string {
+		if (!avatarHash) {
+			const index = Number(BigInt(userId) >> 22n) % 6
+
+			return cdn.defaultAvatar(index) // TODO: error handling
+		}
+
+		return cdn.avatar(userId, avatarHash)
+	}
+
+}
