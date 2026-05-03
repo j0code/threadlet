daf772c37c970de51d009e47756fa80b7fd5d732
cfp <44261356+cfpwastaken@users.noreply.github.com>
2026-05-03 19:31:42 +0200

    Update client/src/comps/Avatar.ts
    Co-authored-by: j0code <42189560+j0code@users.noreply.github.com>

--- client/src/comps/Avatar.ts
+++ client/src/comps/Avatar.ts
@@ -47,7 +47,7 @@ export default class Avatar extends Component {
 			hash = str.charCodeAt(i) + ((hash << 5) - hash)
 		}
 		const color = (hash & 0x00FFFFFF).toString(16).toUpperCase()
-		return "#" + "00000".substring(0, 6 - color.length) + color
+		return `#${color.padStart(6, "0")}`
 	}
 
 	private contrastingColor(hex: string): "#000000" | "#FFFFFF" {
