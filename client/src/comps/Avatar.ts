31ca1abd91490f0457da520c75ae95a57ff68480
cfp <44261356+cfpwastaken@users.noreply.github.com>
2026-05-04 08:15:19 +0200

    Update client/src/comps/Avatar.ts
    Co-authored-by: j0code <42189560+j0code@users.noreply.github.com>

--- client/src/comps/Avatar.ts
+++ client/src/comps/Avatar.ts
@@ -56,9 +56,9 @@ export default class Avatar extends Component {
 	}
 
 	private contrastingColor(hex: string): "#000000" | "#FFFFFF" {
-		const r = parseInt(hex.substr(1, 2), 16)
-		const g = parseInt(hex.substr(3, 2), 16)
-		const b = parseInt(hex.substr(5, 2), 16)
+		const r = parseInt(hex.substring(1, 3), 16)
+		const g = parseInt(hex.substring(3, 5), 16)
+		const b = parseInt(hex.substring(5, 7), 16)
 		const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
 		return luminance > 0.5 ? "#000000" : "#FFFFFF"
 	}
