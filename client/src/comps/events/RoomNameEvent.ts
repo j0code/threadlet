94930bbbf873e36e5c3401878e994d4a8679b6d5
j0code <42189560+j0code@users.noreply.github.com>
2026-05-11 22:44:49 +0200

    feat: enhance member and room event displays with twemoji support

--- client/src/comps/events/RoomNameEvent.ts
+++ client/src/comps/events/RoomNameEvent.ts
@@ -1,6 +1,8 @@
 import { MatrixEvent } from "matrix-js-sdk"
 import EventMessageBase from "./EventMessageBase"
-import { matrix } from "../../matrix"
+import { getMXUser } from "../../matrix"
+import { twemojiParse } from "../../md"
+import { parseEventContent } from "../../events"
 
 export default class RoomNameEvent extends EventMessageBase {
 	constructor(msg: MatrixEvent) {
@@ -11,7 +13,16 @@ export default class RoomNameEvent extends EventMessageBase {
 	async reset() {
 		await super.reset()
 
-		const author = matrix.getUser(this.message.getSender() || "")
-		this.contentElement.textContent = `${author?.displayName || this.message.getSender()} changed the room name to ${this.message.getContent().name}`
+		const { mxid, displayname } = await getMXUser(this.message.getSender()!)
+		const authorName = twemojiParse(
+			displayname || mxid || "Unknown User"
+		)
+
+		const content = parseEventContent(this.message.getContent())
+		// @ts-expect-error content.name should be string
+		const roomName = twemojiParse(content.name || "Unknown")
+
+		
+		this.contentElement.innerHTML = `${authorName} changed the room name to ${roomName}`
 	}
 }
