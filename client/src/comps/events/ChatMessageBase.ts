6918f61d6bbfd83182b2f0a5af378b1d5ebce605
j0code <42189560+j0code@users.noreply.github.com>
2026-05-11 22:08:24 +0200

    refactor(events): consolidate common DOM elements in EventBase.ts

--- client/src/comps/events/ChatMessageBase.ts
+++ client/src/comps/events/ChatMessageBase.ts
@@ -1,42 +1,26 @@
 import { twemojiParse } from "../../md"
 import { MatrixEvent } from "matrix-js-sdk"
 import { getMXUser } from "../../matrix"
-import Avatar from "../Avatar"
 import EventBase from "./EventBase"
+import { relativeTimeFormat } from "../../util"
 
 export default class ChatMessageBase extends EventBase {
-	readonly contentElement: HTMLDivElement
-	readonly avatarElement: Avatar
 	readonly nameElement: HTMLSpanElement
-	readonly timestampElement: HTMLTimeElement
 
 	constructor(msg: MatrixEvent) {
 		super(msg, "div", { id: `message-${msg.getId()}`, classes: ["message"] })
 
-		this.contentElement = document.createElement("div")
-		this.contentElement.className = "message-content md"
-
-		this.avatarElement = new Avatar(msg.getSender()!, "message-author-avatar")
-
 		this.nameElement = document.createElement("span")
 		this.nameElement.className = "message-author-name"
-
-		this.timestampElement = document.createElement("time")
-		this.timestampElement.className = "message-timestamp"
-
-		const aside = document.createElement("div")
-		aside.className = "message-aside"
-		aside.append(this.avatarElement.element)
 
 		const header = document.createElement("div")
 		header.className = "message-header"
 		header.append(this.nameElement, this.timestampElement)
 
-		const main = document.createElement("div")
-		main.className = "message-main"
-		main.append(header, this.contentElement)
+		this.mainElement.append(header, this.contentElement)
 
-		this.element.append(aside, main)
+		const content = this.message.getContent()
+		this.element.dataset.msgtype = content.msgtype || "m.text"
 
 		void this.reset()
 	}
@@ -49,7 +33,7 @@ export default class ChatMessageBase extends EventBase {
 		)
 		this.timestampElement.dateTime = msg.getDate()?.toISOString() || ""
 		// this.timestampElement.textContent = msg.getDate()?.toISOString() || ""
-		this.timestampElement.textContent = this.relativeTimeFormat(
+		this.timestampElement.textContent = relativeTimeFormat(
 			msg.getDate() || new Date()
 		)
 
@@ -58,21 +42,5 @@ export default class ChatMessageBase extends EventBase {
 			.forEach(el => {
 				el.addEventListener("click", () => (el.style.filter = "none"))
 			})
-	}
-
-	relativeTimeFormat(date: Date) {
-		const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" })
-		const now = new Date()
-		const diff = (date.getTime() - now.getTime()) / 1000
-
-		if (Math.abs(diff) < 60) {
-			return rtf.format(Math.round(diff), "second")
-		} else if (Math.abs(diff) < 3600) {
-			return rtf.format(Math.round(diff / 60), "minute")
-		} else if (Math.abs(diff) < 86400) {
-			return rtf.format(Math.round(diff / 3600), "hour")
-		} else {
-			return rtf.format(Math.round(diff / 86400), "day")
-		}
 	}
 }
