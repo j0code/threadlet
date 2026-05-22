e482a832a1c514145726827c0a05ca1a66fae78c
Jannik <jannik@picoscratch.de>
2026-05-22 15:06:41 +0200

    feat(ContextMenu): overhaul

--- client/src/comps/ForumTab.ts
+++ client/src/comps/ForumTab.ts
@@ -12,25 +12,27 @@ export default class ForumTab extends Component {
 	constructor(forum: Room) {
 		super("div", { classes: ["list-tab"] })
 
-		const ctxMenu = new ContextMenu("div", { classes: ["forum-tab-menu"] })
+		const nameEl = document.createElement("span")
+		nameEl.textContent = twemojiParse(forum.name)
+		this.tab = nameEl
 
-		const leaveButton = document.createElement("div")
-		leaveButton.textContent = "Leave"
-		leaveButton.addEventListener("click", () => {
-			new ConfirmForm().openModal(
-				`Are you sure you want to leave ${forum.name}?`,
-				async (confirmed: boolean) => {
-					if (!confirmed) return
-					await matrix.leave(forum.roomId)
-					app.updateChannelList()
-					app.clearView()
+		const ctxMenu = new ContextMenu("div", { classes: ["forum-tab-menu"] }, nameEl)
+		ctxMenu.reset([
+			{
+				label: "Leave",
+				action: () => {
+					new ConfirmForm().openModal(
+						`Are you sure you want to leave ${forum.name}?`,
+						async (confirmed: boolean) => {
+							if (!confirmed) return
+							await matrix.leave(forum.roomId)
+							app.updateChannelList()
+							app.clearView()
+						}
+					)
 				}
-			)
-		})
-		ctxMenu.content.appendChild(leaveButton)
-
-		this.tab = ctxMenu.trigger
-		ctxMenu.trigger.innerHTML = twemojiParse(forum.name)
+			}
+		])
 
 		this.element.appendChild(ctxMenu.element)
 	}
