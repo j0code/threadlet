c78f8eef2bab34195368809a16766d47c783950e
Jannik <jannik@picoscratch.de>
2026-05-22 14:57:34 +0200

    feat(ChatMessageBase): toggle spoilers

--- client/src/comps/events/ChatMessageBase.ts
+++ client/src/comps/events/ChatMessageBase.ts
@@ -36,7 +36,7 @@ export default class ChatMessageBase extends EventBase {
 		this.contentElement
 			.querySelectorAll<HTMLSpanElement>("[data-mx-spoiler]")
 			.forEach(el => {
-				el.addEventListener("click", () => el.classList.add("revealed"))
+				el.addEventListener("click", () => el.classList.toggle("revealed"))
 			})
 	}
 }
